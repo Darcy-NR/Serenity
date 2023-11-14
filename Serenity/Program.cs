22b5bedfcf36f5710f6b18f24300f1f388005c40
Darcy-NR <darcynic@live.com.au>
2023-11-14 16:24:26 +1100

    Init Commit
    Building strong healthy bones in Serenity, assembled some classes got started on the API handler class, gonna need to check up on handling that though because I need some test users and stuff to work with.

--- Serenity/Program.cs
+++ Serenity/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serenity;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        User testUser = new User("1234", "password", "email@email.com");
+
+        Console.WriteLine("User Receipt: ");
+        Console.WriteLine(testUser.getReceiptID());
+        Console.WriteLine("User Password: ");
+        Console.WriteLine(testUser.getPassword());
+        Console.WriteLine("User Email: ");
+        Console.WriteLine(testUser.getEmail());
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Serenity/User.cs
@@ -0,0 +1,51 @@
+﻿namespace Serenity
+{
+    public class User
+    {
+        public string receiptID;
+        public string password;
+        public string email;
+
+        // Default constructor //
+        public User(string receiptID, string password, string email)
+        {
+            this.receiptID = receiptID;
+            this.password = password;
+            this.email = email;
+        }
+
+
+        // Getters and Setters //
+
+        public string getReceiptID()
+        {
+            return receiptID;
+        }
+
+        public void getReceiptID(string value)
+        {
+            receiptID = value;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public void setPassword(string value)
+        {
+            password = value;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public void setEmail(string value)
+        {
+            email = value;
+        }
+
+    }
+}
